4894a36434ba12e2f32dc37cae0a127c4eef5797
Katie Seggelke <katie.seggelke@gmail.com>
2019-09-19 22:20:46 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,8 @@ class Triangle
   
   def kind 
     if side_1_length + side_2_length < side_3_length || side_3_length + side_2_length < side_1_length || side_1_length + side_3_length < side_2_length || side_1_length <= 0 || side_2_length <= 0 || side_3_length <=0
-    if side_1_length == side_2_length && side_1_length == side_3_length
+      raise TriangleError
+    elsif side_1_length == side_2_length && side_1_length == side_3_length
       return :equilateral
     elsif side_1_length == side_2_length || side_2_length == side_3_length || side_1_length == side_3_length
       return :isosceles
