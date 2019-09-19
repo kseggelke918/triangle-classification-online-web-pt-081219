27ad280e23e730e674e1b1786b2df9975756019c
Katie Seggelke <katie.seggelke@gmail.com>
2019-09-19 21:58:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,13 +9,13 @@ class Triangle
   
   def kind 
     if side_1_length == side_2_length && side_1_length == side_3_length
-      puts "equilateral"
+      :equilateral
     end 
     if side_1_length == side_2_length || side_2_length == side_3_length || side_1_length == side_3_length
-      puts "isoscles"
+      :isosceles
     end 
     if side_1_length != side_2_length && side_1_length != side_3_length && side_2_length != side_3_length
-      puts "scalene"
+      :scalene
     end 
     if side_1_length + side_2_length < side_3_length || side_3_length + side_2_length < side_1_length || side_1_length + side_3_length < side_2_length
         begin 
