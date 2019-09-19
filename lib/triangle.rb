395fe826676c4b330914eec1cdc14671586ffbbf
Katie Seggelke <katie.seggelke@gmail.com>
2019-09-19 22:20:30 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,18 +8,19 @@ class Triangle
   end 
   
   def kind 
+    if side_1_length + side_2_length < side_3_length || side_3_length + side_2_length < side_1_length || side_1_length + side_3_length < side_2_length || side_1_length <= 0 || side_2_length <= 0 || side_3_length <=0
     if side_1_length == side_2_length && side_1_length == side_3_length
       return :equilateral
     elsif side_1_length == side_2_length || side_2_length == side_3_length || side_1_length == side_3_length
       return :isosceles
     elsif side_1_length != side_2_length && side_1_length != side_3_length && side_2_length != side_3_length
       return :scalene
-    elsif side_1_length + side_2_length < side_3_length || side_3_length + side_2_length < side_1_length || side_1_length + side_3_length < side_2_length || side_1_length <= 0 || side_2_length <= 0 || side_3_length <=0 
-          puts "this should be the error message"
-        # begin 
-        # raise TriangleError
-        # rescue TriangleError
-        #   puts error.message
+    # elsif side_1_length + side_2_length < side_3_length || side_3_length + side_2_length < side_1_length || side_1_length + side_3_length < side_2_length || side_1_length <= 0 || side_2_length <= 0 || side_3_length <=0 
+    #       puts "this should be the error message"
+    #     # begin 
+    #     # raise TriangleError
+    #     # rescue TriangleError
+    #     #   puts error.message
     else 
        "Is this a triange???"
     end 
