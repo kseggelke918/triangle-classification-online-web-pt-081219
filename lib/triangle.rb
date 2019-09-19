884b760511ff039cfb8800101ec7062980713ac1
Katie Seggelke <katie.seggelke@gmail.com>
2019-09-19 22:14:13 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,6 @@ class Triangle
       return :isosceles
     elsif side_1_length != side_2_length && side_1_length != side_3_length && side_2_length != side_3_length
       return :scalene
-    end 
     elsif side_1_length + side_2_length < side_3_length || side_3_length + side_2_length < side_1_length || side_1_length + side_3_length < side_2_length || side_1_length <= 0 || side_2_length <= 0 || side_3_length <=0 
         begin 
           raise TriangleError
