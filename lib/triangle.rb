4de73881229370467078a6b77c9e95445a611ec1
Katie Seggelke <katie.seggelke@gmail.com>
2019-09-19 21:41:00 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,17 @@
 class Triangle
-  # write code here
+  attr_accessor :equilateral, :isosceles, :scalene
+  
+  def initialize(equilateral, isosceles, scalene)
+    @equilateral = equilateral
+    @isosceles = isosceles
+    @scalene = scalene
+  end 
+  
+  def kind 
+    # raise TriangleError if triange.valid = false 
+  end 
+  
+  class TriangleError < StandardError
+  end 
+  
 end
