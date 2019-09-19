710a215dc59116fe58c3d5881f1ca6d364380ee8
Katie Seggelke <katie.seggelke@gmail.com>
2019-09-19 22:19:33 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,6 +8,7 @@ class Triangle
   end 
   
   def kind 
+    if 
     if side_1_length == side_2_length && side_1_length == side_3_length
       return :equilateral
     elsif side_1_length == side_2_length || side_2_length == side_3_length || side_1_length == side_3_length
